81eeee706b7871f9a3acf1afbcc0f029a34360c5
Andre <andre@aaindustries.de>
2021-11-24 22:31:22 +0100

    Initial Commit

---
+++ GCC-Helper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GCC-Helper
+//
+//  Created by Andre Albach on 24.11.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
